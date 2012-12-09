7987757c8fed1aac1acebf1d132c6f4005bc7677
magic9 <magic9@magic9.(none)>
2012-12-09 02:34:06 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '1aae42db60048598551a7b3fed3176d4aa8a270b6fc09cde9b2d5063fbc660b3bfaddbaea98210b9a5ff9e0607ea9799582694805ff83ecf38bed102c98454f8'
